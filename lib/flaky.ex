33f84824c0caa5eaa22b81efba29b4509e095b62
Nirvana Platform <hello@nirvana.io>
2013-03-26 21:23:50 -0300

    partial implementation

--- lib/flaky.ex
+++ lib/flaky.ex
@@ -1,4 +1,8 @@
-defrecord FlakyState, node: nil, time: 0
+
+# Node is the id of this node (based on MAC address)
+# Time is the stateful time (to tell if we're called more than once during time resolution period)
+# SQ is the sequence number which is incremented so that multiple calls in a time slice will generate unique ids.
+defrecord FlakyState, node: nil, time: 0, sq: 0
 
 defmodule Flaky do
 	use Application.Behaviour
@@ -19,11 +23,13 @@ defmodule Flaky do
 	def mac(name) do
 		{:ok, addresses} = :inet.getifaddrs()
 		proplist = :proplists.get_value(name, addresses)
-		case proplist do
-			:undefined -> {:error, :interface_not_found}
-			_ -> :proplists.get_value(:hwaddr, proplist)
-		end
+		hwaddr = :proplists.get_value(:hwaddr, proplist)
+		<<worker::[integer, size(48)]>> = list_to_binary(hwaddr)
+		worker
 	end
+# hw_addr_to_int(HwAddr) ->
+#    <<WorkerId:48/integer>> = erlang:list_to_binary(HwAddr),
+#    WorkerId.
 
 
 end
--- lib/flaky/server.ex
+++ lib/flaky/server.ex
@@ -9,11 +9,47 @@ defmodule Flaky.Server do
     { :ok, state }
   end
 
-  def handle_call(:pop, _from, [h|stack]) do
-    { :reply, h, stack }
+
+#defrecord FlakyState, node: nil, time: 0, sq: 0
+
+  def handle_call(:get, _from, state) do
+    { flake, new_state} = get(Flake.time, state, 10)
+    { :reply, flake, new_state }
   end
 
-  def handle_cast({ :push, new }, stack) do
+  def handle_call({:get, base} _from, state) do
+    { flake, new_state} = get(Flake.time, state, base)
+    { :reply, flake, new_state }
+  end
+
+
+  # Matches when the calling time is the same as the state time. Incr. sq
+  def get(time, FlakyState[time:time, node:node, sq:seq], base) do
+    new_state = FlakyState.new(time:time, node:node, sq:seq+1)
+    number = gen_flake(new_state)
+    flake = integer_to_list(number, base)
+    {flake, new_state}
+  end
+
+  # Matches when the times are different, reset sq
+  def get(newtime, FlakyState[time:time, node:node, sq:seq], base)  when newtime > time do
+    new_state = FlakyState.new(time:newtime, node:node, sq:0)
+    number = gen_flake(new_state)
+    flake = integer_to_list(number, base)
+    {flake, new_state}
+  end 
+
+  # Error when clock is running backwards
+  def get(newtime, FlakyState[time:time, node:node, sq:seq], base) when newtime < time do
+    {:error, :clock_running_backwards}
+  end
+
+  def gen_flake() do
+
+  end
+
+def handle_cast({ :push, new }, stack) do
     { :noreply, [new|stack] }
   end
+
 end
