20d906d6e5fe65d66ffe08af63d37954199b7a22
Nirvana Platform <hello@nirvana.io>
2013-04-05 18:30:48 -0300

    Updated package.exs to work better with the expm directory.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
-Expm.Package.new(name: "Flaky", description: "A decentralized, k-ordered id generation service in Elixir (based on Flake)",
+Expm.Package.new(name: "flaky", description: "A decentralized, k-ordered id generation service in Elixir (based on Flake)",
                  version: "0.1", keywords: ["Elixir", "Flake", "key", "distributed", "library"], 
-                 homepage: "www.nirvana.io",
+                 homepage: "nirvana.io",
                  maintainers: [[name: "Nirvana Organization", 
                                 email: "hello@nirvana.io"]],
-                 repositories: [[github: "NirvanaPlatform/flaky"]])
+                 repositories: [[github: "nirvana/flaky"]])
     
