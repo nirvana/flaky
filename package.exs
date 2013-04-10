872006421e79c08eef603ef9212c48a4944abe37
Nirvana Platform <hello@nirvana.io>
2013-04-10 17:01:31 -0300

    Fixed url in license

--- package.exs
+++ package.exs
@@ -1,4 +1,4 @@
-Expm.Package.new(name: "flaky", description: "A decentralized, k-ordered id generation service in Elixir (based on Flake)",
+Expm.Package.new(name: "flaky", description: "A decentralized, k-ordered id generation service in Elixir / Erlang (based on Flake)",
                  version: "0.1", keywords: ["Elixir", "Flake", "key", "distributed", "library"], 
                  homepage: "nirvana.io",
                  maintainers: [[name: "Nirvana Organization", 
