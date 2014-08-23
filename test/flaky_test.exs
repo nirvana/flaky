dad5729f90632ee8a57b4d83c3ba955f59f6ec80
Nirvana <hello@nirvana.io>
2014-08-23 14:53:26 -0500

    moved test function into unit tests

--- test/flaky_test.exs
+++ test/flaky_test.exs
@@ -2,8 +2,10 @@ defmodule FlakyTest do
   use ExUnit.Case
 
   setup do
+    start = Flaky.Servertime
     flakes = Enum.map 1..10_000, fn(_) -> Flaky.generate(62) end
-    {:ok, [flakes: flakes]}
+    finish = Flaky.Server.time
+    {:ok, [flakes: flakes, start: start, finish: finish]}
   end
 
   test "flakes are generated in sequence", %{flakes: flakes} do
@@ -12,6 +14,12 @@ defmodule FlakyTest do
 
   test "flakes are uniq", %{flakes: flakes} do
     assert flakes == Enum.uniq(flakes)
+  end
+
+  test "flakes generated quickly", %{flakes: flakes, start: start, finish: finish} do
+    time = finish - start
+    IO.puts "Time to generate #{Enum.count flakes} flakes: #{time} milliseconds."
+    assert time <= 1000
   end
 
 end
