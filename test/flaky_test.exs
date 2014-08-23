bba8b32fa84f28fa3bdaa45b58494c981275d661
Nirvana <hello@nirvana.io>
2014-08-23 15:56:05 -0500

    fixed tests

--- test/flaky_test.exs
+++ test/flaky_test.exs
@@ -2,10 +2,16 @@ defmodule FlakyTest do
   use ExUnit.Case
 
   setup do
-    start = Flaky.Servertime
+    start = Flaky.Server.time
     flakes = Enum.map 1..10_000, fn(_) -> Flaky.generate(62) end
     finish = Flaky.Server.time
     {:ok, [flakes: flakes, start: start, finish: finish]}
+  end
+
+  test "flakes generated quickly", %{start: start, finish: finish} do
+    time = finish - start
+    IO.puts "Time to generate 10,000 flakes: #{time} milliseconds."
+    assert time <= 500
   end
 
   test "flakes are generated in sequence", %{flakes: flakes} do
@@ -14,12 +20,6 @@ defmodule FlakyTest do
 
   test "flakes are uniq", %{flakes: flakes} do
     assert flakes == Enum.uniq(flakes)
-  end
-
-  test "flakes generated quickly", %{flakes: flakes, start: start, finish: finish} do
-    time = finish - start
-    IO.puts "Time to generate #{Enum.count flakes} flakes: #{time} milliseconds."
-    assert time <= 1000
   end
 
 end
