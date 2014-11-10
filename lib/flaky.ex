09da5f38012306b179d6c36e36e843bcbbc12d8e
Akash Manohar J <akash@akash.im>
2014-11-10 22:55:47 +0530

    Define supervisor in Flaky module; Remove explicit Flaky.Supervisor definition

--- lib/flaky.ex
+++ lib/flaky.ex
@@ -1,8 +1,21 @@
 defmodule Flaky do
+  use Application
 
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
   def start(_type, _args) do
-    Flaky.Supervisor.start_link
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Flaky.Server, [])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Flaky.Supervisor]
+    Supervisor.start_link(children, opts)
   end
+
 
   def generate(base) do
     Flaky.Server.generate(base)
